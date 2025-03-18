a0524ddd60d33eefffc1944d401149ef11921d15
Sir.MmD <27060102+Sir-MmD@users.noreply.github.com>
2025-03-19 00:01:45 +0330

    Update Reconfig_Paths.bat

--- Reconfig_Paths.bat
+++ Reconfig_Paths.bat
@@ -20,5 +20,9 @@ powershell -NoProfile -Command ^
   "(Get-Content '%PCSX2_INI%') -replace '^\s*Bios\s*=.*$', 'Bios=%SCRIPT_DIR%data\Bios' -replace '^\s*Snapshots\s*=.*$', 'Snapshots=%SCRIPT_DIR%data\Snapshots' -replace '^\s*Savestates\s*=.*$', 'Savestates=%SCRIPT_DIR%data\Savestates' -replace '^\s*MemoryCards\s*=.*$', 'MemoryCards=%SCRIPT_DIR%data\MemoryCards' -replace '^\s*Logs\s*=.*$', 'Logs=%SCRIPT_DIR%data\Logs' -replace '^\s*Cheats\s*=.*$', 'Cheats=%SCRIPT_DIR%data\Cheats' -replace '^\s*Patches\s*=.*$', 'Patches=%SCRIPT_DIR%data\Patches' -replace '^\s*UserResources\s*=.*$', 'UserResources=%SCRIPT_DIR%data\UserResources' -replace '^\s*Cache\s*=.*$', 'Cache=%SCRIPT_DIR%data\Cache' -replace '^\s*Textures\s*=.*$', 'Textures=%SCRIPT_DIR%TEXTURE' -replace '^\s*InputProfiles\s*=.*$', 'InputProfiles=%SCRIPT_DIR%data\InputProfiles' -replace '^\s*Videos\s*=.*$', 'Videos=%SCRIPT_DIR%data\Videos' -replace '^\s*RecursivePaths\s*=.*$', 'RecursivePaths=%SCRIPT_DIR%ROM' -replace '^\s*Covers\s*=.*$', 'Covers=%SCRIPT_DIR%data\Covers' | Set-Content '%PCSX2_INI%'"
 
 echo PCSX2.ini has been modified successfully.
-pause
+echo;
+echo [92m DONE[0m 
+echo;
+echo Press any key to exit
+pause > nul
 endlocal
